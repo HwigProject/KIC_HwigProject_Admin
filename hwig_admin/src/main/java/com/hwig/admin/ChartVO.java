08bf2f199da930f5264254d007fbd6d7f82bbd10
dnjstjq25 <dnjstjq255@gmail.com>
2020-05-25 16:38:22 +0900

    0525 -8

--- hwig_admin/src/main/java/com/hwig/admin/ChartVO.java
+++ hwig_admin/src/main/java/com/hwig/admin/ChartVO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.hwig.admin;
 
 public class ChartVO {
@@ -21,27 +20,3 @@ public class ChartVO {
 		this.category_id = category_id;
 	}
 }
-=======
-package com.hwig.admin;
-
-public class ChartVO {
-	private int order_paymoney;
-	private String category_id;
-
-	public int getOrder_paymoney() {
-		return order_paymoney;
-	}
-
-	public void setOrder_paymoney(int order_paymoney) {
-		this.order_paymoney = order_paymoney;
-	}
-
-	public String getCategory_id() {
-		return category_id;
-	}
-
-	public void setCategory_id(String category_id) {
-		this.category_id = category_id;
-	}
-}
->>>>>>> branch 'hwig_lws' of https://github.com/HwigProject/KIC_HwigProject_Admin.git
